621d599c05eeb01bab2ab7181ad9aa5003359bcc
Akhilesh <360akhileshsingh.it@gmail.com>
2024-01-15 17:21:01 +0530

    Using async methods when querying a database.

--- ReStore/API/Controllers/ProductsController.cs
+++ ReStore/API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace API.Controllers
@@ -14,24 +15,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext context;
+        private readonly StoreContext _context;
         public ProductsController(StoreContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = context.Products.ToList();
-            return Ok(products);
+            
+            return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")]
 
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return context.Products.Find(id);
+            return await _context.Products.FindAsync(id);
         }
     }
 }
