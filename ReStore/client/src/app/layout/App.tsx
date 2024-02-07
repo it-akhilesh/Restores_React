0d916c00680ee3dcd7a01227c3c38ec80d8f5d2a
Akhilesh <360akhileshsingh.it@gmail.com>
2024-02-07 22:02:58 +0530

    Adding toast notification.

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -1,9 +1,11 @@
-import Catalog from "../../features/catalog/Catalog";
+
 import Header from "./Header";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Container, ThemeProvider, createTheme } from "@mui/material";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() { 
@@ -23,6 +25,7 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
