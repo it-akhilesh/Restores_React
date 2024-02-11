c6dcb84fd09a551ab70ede92a5eb31a9cb0a0bae
Akhilesh <360akhileshsingh.it@gmail.com>
2024-02-11 21:34:30 +0530

    Shaping the data to return .

---
+++ ReStore/API/DTOs/BasketDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class BasketDto
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+    }
+
+    public class BasketItemDto
+    {
+        public int productId { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+    }
+}
