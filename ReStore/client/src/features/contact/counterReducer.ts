b958b8aa40effd2f34d1819ae73e06fda7ab60e6
Akhilesh <360akhileshsingh.it@gmail.com>
2024-02-25 17:10:27 +0530

    Installing and using Redux.

--- ReStore/client/src/features/contact/counterReducer.ts
+++ ReStore/client/src/features/contact/counterReducer.ts
@@ -1,3 +1,7 @@
+export const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+export const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+
+
 export interface CounterState {
     data: number;
     title: string;
@@ -9,5 +13,21 @@ const initialState: CounterState = {
 }
 
 export default function counterReducer(state = initialState, action: any) {
+    switch (action.type) {
+        case INCREMENT_COUNTER:
+            return {
+                ...state,
+                data: state.data + 1
+            }
+            case DECREMENT_COUNTER:
+                return {
+                    ...state,
+                    data: state.data - 1
+                }
+           
+            default:
+                break;
+    }
+
     return state;
 }
