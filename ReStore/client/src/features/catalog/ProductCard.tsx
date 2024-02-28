138b875130df4ef109f0117db8f2ed22fa40fc57
Akhilesh <360akhileshsingh.it@gmail.com>
2024-02-28 13:48:39 +0530

    Using async functions in rdux part 2.

--- ReStore/client/src/features/catalog/ProductCard.tsx
+++ ReStore/client/src/features/catalog/ProductCard.tsx
@@ -1,28 +1,18 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
-import { useStoreContext } from "../../app/context/StoreContext";
 import { currencyFormat } from "../../app/util/util";
-import { useAppDispatch } from "../../app/store/configureStore";
-import { setBasket } from "../basket/basketSlice";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
 interface Props {
     product: Product;
 }
-export default function ProductCard({ product }: Props) {
-    const [loading, setLoading] = useState(false);
+export default function ProductCard({ product }: Props) 
+{
+    const {status} = useAppSelector(state => state.basket);
     const dispatch = useAppDispatch();
-
-    function handleAddItem(productId: number) {
-        setLoading(true);
-        agent.Basket.addItem(productId)
-            .then(basket => dispatch(setBasket(basket)))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false));
-    }
     return (
         <Card>
             <CardHeader
@@ -51,12 +41,15 @@ export default function ProductCard({ product }: Props) {
             </CardContent>
             <CardActions>
                 <LoadingButton
-                    loading={loading}
-                    onClick={() => handleAddItem(product.id)}
+                    loading={status.includes('pendingAddItem' + product.id)}
+                    onClick={() => dispatch(addBasketItemAsync({productId: product.id}))}
                     size="small">Add to cart
                 </LoadingButton>
                 <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
+
+    
 }
+    
