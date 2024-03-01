15c80c9b47395f549806d3b43fdc0e5fcf4ab718
Akhilesh <360akhileshsingh.it@gmail.com>
2024-03-01 23:37:18 +0530

    Using Entity Adapters,

---
+++ ReStore/client/src/features/catalog/catalogSlice.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
+import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
+
+const productsAdapter = createEntityAdapter<Product>();
+
+export const fetchProductsAsync = createAsyncThunk<Product[]>(
+    'catalog/fetchProductsAsync',
+    async () => {
+        try {
+            return await agent.Catalog.list()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+)
+
+export const catalogSlice = createSlice({
+    name: 'catalog',
+    initialState: productsAdapter.getInitialState({
+        productsLoaded: false,
+        status: 'idle'
+    }),
+    reducers: {}
+})
