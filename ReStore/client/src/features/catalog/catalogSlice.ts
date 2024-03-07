82e6f793da21e89d0e74a7261ffe2369ca491750
Akhilesh <360akhileshsingh.it@gmail.com>
2024-03-07 16:55:18 +0530

    Using entity adapter selectors.

--- ReStore/client/src/features/catalog/catalogSlice.ts
+++ ReStore/client/src/features/catalog/catalogSlice.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
+import { RootState } from "../../app/store/configureStore";
 
 const productsAdapter = createEntityAdapter<Product>();
 
@@ -21,5 +22,20 @@ export const catalogSlice = createSlice({
         productsLoaded: false,
         status: 'idle'
     }),
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder => {
+        builder.addCase(fetchProductsAsync.pending, (state) => {
+            state.status = 'pendingFetchProducts';
+        });
+        builder.addCase(fetchProductsAsync.fulfilled, (state, action) => {
+            productsAdapter.setAll(state, action.payload);
+            state.status = 'idle';
+            state.productsLoaded = true;
+        });
+        builder.addCase(fetchProductsAsync.rejected, (state) => {
+            state.status = 'idle';
+        })
+    })
 })
+
+export const productSelectors = productsAdapter.getSelectors((state: RootState) => state.catalog);
--- ReStore/client/src/index.tsx
+++ ReStore/client/src/index.tsx
@@ -8,10 +8,10 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes.js';
-import { StoreProvider } from './app/context/StoreContext.js';
 
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore.js';
+import { fetchProductsAsync } from './features/catalog/catalogSlice.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -25,5 +25,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     
   </React.StrictMode>
 );
+store.dispatch(fetchProductsAsync());
 
 
